a16070b2cb97d340598679320728cf93a3fa6a4e
maxwalks <78441835+maxwalks@users.noreply.github.com>
2021-08-07 15:51:47 +0200

    changed something in gitignore and index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('dotenv').config()
 const Discord= require('discord.js');
 const client = new Discord.Client({ intents: ['GUILD_MESSAGES', 'GUILDS'] });
 const prefix = '.';
-const config = require('./config.json');
 
 client.on('ready', ()=> {
     console.log('Bot is ready!')
